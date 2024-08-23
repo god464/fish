ed11d1a3c7a9071afb2b94a6ca4db63236ca828c
god464 <god464@users.noreply.github.com>
2024-08-23 15:43:53 +0800

    adding support for opam

--- config.fish
+++ config.fish
@@ -8,3 +8,12 @@ if status is-interactive
     abbr -a man batman
     abbr -a diff batdiff
 end
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/cat/.opam/opam-init/init.fish' && source '/home/cat/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
